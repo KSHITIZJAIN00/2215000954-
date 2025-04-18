509b408580e53cec54eaf1d08df7593f6318e146
KSHITIZJAIN00 <114683009+KSHITIZJAIN00@users.noreply.github.com>
2025-04-18 13:07:52 +0530

    Update index.js

--- question-1/server/index.js
+++ question-1/server/index.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = 9876;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:9876`);
 });
